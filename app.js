7cdffd2c2b6f5bdb632d30636bb81be76f6dac55
martin <martin.terzi@acquappessa.com.ar>
2021-10-31 07:30:14 -0200

    cambio2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
-// https://github.com/martinterzi/infosys.git
+
 const app = express();
 const port = process.env.PORT || 4000;
 
