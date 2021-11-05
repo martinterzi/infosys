e5a2a904081d1def5b04a76795f80b3c10b5c53a
martin <martin.terzi@acquappessa.com.ar>
2021-11-05 05:23:00 -0200

    cnov36

--- database/db.js
+++ database/db.js
@@ -1,17 +1,18 @@
 const mysql = require('mysql');
 
-const con = mysql.createConnection({
+const con = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
 })
 
-con.connect((error)=>{
+con.getConnection((error, connection)=>{
     if(error){
         console.log('el error a conectar es'+error)
     } else{
         console.log('base conectada')
+        connection.release();
     }
 });
 
