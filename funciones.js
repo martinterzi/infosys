ff6159f307980cd8b8edadc1bf7383c2e8042fdd
martin <martin.terzi@acquappessa.com.ar>
2021-11-08 23:08:17 -0200

    cnov85

--- funciones.js
+++ funciones.js
@@ -1,7 +1,7 @@
 let editable = document.getElementById('editable');
 let fdni = document.getElementById('fdni');
 
-editable.addEventListener("change", activarEdit);
+editable.addEventListener("change", activarEdit());
 
 function activarEdit(){
  fdni.disabled = false;
