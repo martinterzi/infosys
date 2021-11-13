5d2c7595804c89e05105ab1ad0e956314eec9227
martin <martin.terzi@acquappessa.com.ar>
2021-11-13 12:57:42 -0200

    cnov136

--- routes/router.js
+++ routes/router.js
@@ -5,6 +5,22 @@ const conexion = require('../database/db');
 const authController = require('../controllers/authController');
 
 
+var multer = require('multer');
+var fecha = Date.now();
+
+var rutaAlmacen = multer.diskStorage(
+    {
+        destination:function(request, file, callback){
+            callback(null, './public/images/');
+        },
+        filename:function(request, file, callback){
+            console.log(file);
+            callback(null, `${fecha}_${file.originalname}`);
+        }
+    }
+);
+
+var cargar = multer({storage:rutaAlmacen});
 
 
 router.get('/', (req, res) => {
@@ -50,21 +66,5 @@ router.post('/actualizarChofer', authController.actualizarChofer);
 router.get('/unidadEliminar/:id', authController.unidadEliminar);
 router.get('/choferEliminar/:id', authController.choferEliminar);
 
-var multer = require('multer');
-var fecha = Date.now();
-
-var rutaAlmacen = multer.diskStorage(
-    {
-        destination:function(request, file, callback){
-            callback(null, './public/images/');
-        },
-        filename:function(request, file, callback){
-            console.log(file);
-            callback(null, `${fecha}_${file.originalname}`);
-        }
-    }
-);
-
-var cargar = multer({storage:rutaAlmacen});
 /* GET home page. */
 module.exports= router;
